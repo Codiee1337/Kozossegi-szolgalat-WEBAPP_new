864c02b166838d86fbed8e426fbc706d10dd9809
Codiee1337 <33905082+Codiee1337@users.noreply.github.com>
2021-06-01 09:42:39 +0200

    Delete backend/api/src/config directory

--- backend/api/src/config/app.ts
+++
@@ -1,7 +0,0 @@
-export const{
-    NODE_ENV = 'development',
-    APP_PORT = 5000,
-    IP_BIND = 'localhost'
-} = process.env
-
-export const IN_PROD = NODE_ENV === 'production'
--- backend/api/src/config/auth.ts
+++
@@ -1,3 +0,0 @@
-export const BCRYPT_WORK_FACTOR = 12
-
-export const BCRYPT_MAXIMUM_BYTES = 72
